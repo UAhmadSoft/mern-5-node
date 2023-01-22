9756c312d4d1834e7264dcd30d773e4de16dbae8
UAhmadSoft <umadahmad1928@gmail.com>
2023-01-22 12:36:17 +0500

    Refactored into controller

---
+++ controllers/userController.js
@@ -0,0 +1,65 @@
+// const createUser = async (req, res) => {
+exports.createUser = async (req, res) => {
+	const { name, age } = req.body;
+	const userData = new User({
+		name,
+		age,
+	});
+
+	try {
+		const savedData = await userData.save();
+		res.status(200).send(savedData);
+	} catch (err) {
+		res.status(400).send({ message: err.message });
+	}
+};
+
+exports.getAllUsers = async (req, res) => {
+	try {
+		const data = await User.find();
+
+		res.send(data);
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+};
+
+exports.getUser = async (req, res) => {
+	try {
+		const data = await User.findById(req.params.id);
+		if (!data) {
+			return res.send('User not found!');
+		}
+		res.send(data);
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+};
+
+exports.updateUser = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const updatedData = req.body;
+		const options = { new: true };
+
+		const result = await User.findByIdAndUpdate(id, updatedData, options);
+
+		res.send(result);
+	} catch (error) {
+		res.status(400).send({ message: error.message });
+	}
+};
+
+exports.deleteUser = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const data = await User.findByIdAndDelete(id);
+		res.send(`Document with ${data.name} has been deleted..`);
+	} catch (error) {
+		res.status(400).send({ message: error.message });
+	}
+};
+
+// module.exports = {
+// 	createUser,
+// };
--- routes/user.js
+++ routes/user.js
@@ -2,67 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../database/models/user');
+const userContoller = require('../controllers/userController');
 
 // /api/users/post POSt
-router.post('/post', async (req, res) => {
-	const { name, age } = req.body;
-	const userData = new User({
-		name,
-		age,
-	});
+router.post('/', userContoller.createUser);
 
-	try {
-		const savedData = await userData.save();
-		res.status(200).send(savedData);
-	} catch (err) {
-		res.status(400).send({ message: err.message });
-	}
-});
+router.get('/', userContoller.getAllUsers);
 
-router.get('/getAll', async (req, res) => {
-	try {
-		const data = await User.find();
+router.get('/:id', userContoller.getUser);
 
-		res.send(data);
-	} catch (error) {
-		res.status(500).send({ message: error.message });
-	}
-});
+router.patch('/:id', userContoller.updateUser);
 
-router.get('/getOne/:id', async (req, res) => {
-	try {
-		const data = await User.findById(req.params.id);
-		if (!data) {
-			return res.send('User not found!');
-		}
-		res.send(data);
-	} catch (error) {
-		res.status(500).send({ message: error.message });
-	}
-});
-
-router.patch('/update/:id', async (req, res) => {
-	try {
-		const id = req.params.id;
-		const updatedData = req.body;
-		const options = { new: true };
-
-		const result = await User.findByIdAndUpdate(id, updatedData, options);
-
-		res.send(result);
-	} catch (error) {
-		res.status(400).send({ message: error.message });
-	}
-});
-
-router.delete('/delete/:id', async (req, res) => {
-	try {
-		const id = req.params.id;
-		const data = await User.findByIdAndDelete(id);
-		res.send(`Document with ${data.name} has been deleted..`);
-	} catch (error) {
-		res.status(400).send({ message: error.message });
-	}
-});
+router.delete('/:id', userContoller.deleteUser);
 
 module.exports = router;
